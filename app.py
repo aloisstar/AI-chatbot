1b93c010727d664037a92e575f3862f534e973ef
Abhishek Singh <131998abhishek@gmail.com>
2026-02-04 12:55:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-
 import os
 import tempfile
 import shutil
 import streamlit as st
 from langchain_groq import ChatGroq
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
